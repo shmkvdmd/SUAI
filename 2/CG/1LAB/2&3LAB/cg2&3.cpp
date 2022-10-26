95142711884c85aa5fa68c5b3b57c5ff9d15d1f7
shmkvdmd <demidka100@mail.ru>
2022-10-26 19:46:31 +0300

    +

--- 2/CG/1LAB/2&3LAB/cg2&3.cpp
+++ 2/CG/1LAB/2&3LAB/cg2&3.cpp
@@ -6,49 +6,116 @@
 double rotate_y = 0;
 double rotate_x = 0;
 double rotate_z = 0;
-GLfloat material_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-GLfloat light0_diffuse[] = { 0.4, 0.7, 0.2 };
-GLfloat light0_position[] = { 0.0, 0.0, 1.0, 1.0 };
+GLfloat mat_amb_diff[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_shininess[] = { 50.0 };
+
+GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat light_diffuse2[] = { 0.0, 1.0, 1.0, 1.0 };
+GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+
+GLfloat light_position[] = { 1.0, 0.0, 0.0, 0.0 };
+GLfloat light_position2[] = { 0.0, 1.0, 0.0, 0.0 };
 void init(void)
 
 {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_SMOOTH);
+
+    static float lmodel_twoside[] = { GL_TRUE };
+    static float lmodel_oneside[] = { GL_FALSE };
+    static float ambient[] = { 0.2, 0.2, 0.2, 1.0 };
+    static float diffuse[] = { 1.0f, 0.0, 0.0, 1.0 };
+    static float position[] = { -2.0, 1.0, 1.0f, 0.0 };
+
+    static float ambient1[] = { 0.3, 0.3, 0.3, 1.0 };
+    static float diffuse1[] = { 0.0, 1.0, 0.0, 1.0 };
+    static float position1[] = { 1.0,1.0, 1.0f, 0.0 };
+
+    static float ambient2[] = { 0.4, 0.4, 0.4, 1.0 };
+    static float diffuse2[] = { 0.0, 0.0, 1.0, 1.0 };
+    static float position2[] = { 2.0,1.0, 1.0f, 0.0 };
+
+
+    static float front_mat_shininess[] =
+    { 60.0 };
+    static float front_mat_specular[] =
+    { 0.2, 0.2, 0.2, 1.0 };
+    static float front_mat_diffuse[] =
+    { 0.5, 0.5, 0.28, 1.0 };
+    static float back_mat_shininess[] =
+    { 60.0 };
+    static float back_mat_specular[] =
+    { 0.5, 0.5, 0.2, 1.0 };
+    static float back_mat_diffuse[] =
+    { 1.0, 0.9, 0.2, 1.0 };
+
+    static float lmodel_ambient[] =
+    { 1.0, 1.0, 1.0, 1.0 };
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
+
+    glLightfv(GL_LIGHT1, GL_AMBIENT, ambient1);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse1);
+    glLightfv(GL_LIGHT1, GL_POSITION, position1);
+
+    glLightfv(GL_LIGHT2, GL_AMBIENT, ambient2);
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, diffuse2);
+    glLightfv(GL_LIGHT2, GL_POSITION, position2);
+
+    glMaterialfv(GL_FRONT, GL_SHININESS, front_mat_shininess);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, front_mat_specular);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, front_mat_diffuse);
+    glMaterialfv(GL_BACK, GL_SHININESS, back_mat_shininess);
+    glMaterialfv(GL_BACK, GL_SPECULAR, back_mat_specular);
+    glMaterialfv(GL_BACK, GL_DIFFUSE, back_mat_diffuse);
+
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+    glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, lmodel_twoside);
+
     glEnable(GL_LIGHTING);
-    glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHT2);
+    glShadeModel(GL_SMOOTH);
 }
+
 void draw()
 {
-    glBegin(GL_POLYGON);
-    glColor3f(0.2, 0.4, 1);
-    glVertex3f(-1, 6, 0.5);
-    glVertex3f(-6, 6, 0.5);
-    glVertex3f(-6, 1, 0.5);
-    glVertex3f(-1, 1, 0.5);
-    glEnd();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glBegin(GL_POLYGON);
-    glColor3f(0.2, 0.4, 1);
-    glVertex3f(6, 6, 0.5);
-    glVertex3f(1, 6, 0.5);
-    glVertex3f(1, 1, 0.5);
-    glVertex3f(6, 1, 0.5);
-    glEnd();
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHT2);
+    glEnable(GL_DEPTH_TEST);
 
-    glBegin(GL_POLYGON);
-    glColor3f(0.2, 0.4, 1);
-    glVertex3f(-1, -6, 0.5);
-    glVertex3f(-6, -6, 0.5);
-    glVertex3f(-6, -1, 0.5);
-    glVertex3f(-1, -1, 0.5);
-    glEnd();
 
-    glBegin(GL_POLYGON);
-    glColor3f(0.2, 0.4, 1);
-    glVertex3f(6, -6, 0.5);
-    glVertex3f(1,-6, 0.5);
-    glVertex3f(1, -1, 0.5);
-    glVertex3f(6, -1, 0.5);
-    glEnd();
+    glPushMatrix();
+    glTranslatef(0.0f, -0.7f, 0.0f);
+    glutSolidCube(0.5);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glutSolidSphere(0.35,20,100);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.0f, 0.7f, 0.0f);
+    glutSolidTorus(0.1,0.2,40,40);
+    glPopMatrix();
+
+
+    glutSwapBuffers();
 }
+
 
 void specialKeys(int key, int x, int y) {
 
@@ -70,15 +137,6 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    glFrustum(-8, 8, -8, 8, 3, 10);		//видовые параметры
-    glTranslatef(0.0, 0.0, -5.0);		//начальный сдвиг системы координат
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
-    glEnable(GL_LIGHT0);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
-    glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
-    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.0);
-    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.2);
-    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.4);
     glRotatef(rotate_x, 1.0, 0.0, 0.0);
     glRotatef(rotate_y, 0.0, 1.0, 0.0);
     draw();
