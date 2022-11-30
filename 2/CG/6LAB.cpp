2c723b51317facacb1b5ac5cc7ff4132d5249f77
shmkvdmd <demidka100@mail.ru>
2022-11-30 21:24:31 +0300

    1

--- 2/CG/6LAB.cpp
+++ 2/CG/6LAB.cpp
@@ -14,11 +14,10 @@ GLUnurbsObj* theNurb;
 const int size_numb_x = 4;
 const int size_numb_y = 4;
 GLuint texture[1];
-bool   gp;                              // G Нажата? ( Новое )
-GLuint filter;                          // Используемый фильтр для текстур
-GLuint fogMode[] = { GL_EXP, GL_EXP2, GL_LINEAR }; // Хранит три типа тумана
-GLuint fogfilter = 0;                    // Тип используемого тумана
-GLfloat fogColor[4] = { 0.5f, 0.5f, 0.5f, 1.0f }; // Цвет тумана
+bool FogFlag = true;                             
+GLuint filter;                          
+GLuint fogfilter = 0;                  
+GLfloat fogColor[3] = { 0.8f, 0.8f, 0.8f }; // Цвет тумана
 GLfloat ctlpoints[size_numb_x][size_numb_y][3];
 GLfloat knots[size_numb_x + size_numb_y] = {
   0.0, 0.0, 0.0, 0.0,
@@ -31,7 +30,87 @@ GLfloat ctrlpoints[4][4][3] = {
 {{-3.5, 1.0,  0.5},  {-0.5, 1.0, 0.5 }, {0.5, 1.0,  0.5 },   {3.5, 1.0, 0.5}},
 {{-3.5, 1.0,  4.5},  {-0.5, 1.0, 4.5 }, {0.5, 1.0,  4.5 },   {3.5, 1.0, 4.5}}
 };
+GLfloat mat_amb_diff[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_shininess[] = { 50.0 };
 
+GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat light_diffuse2[] = { 0.0, 1.0, 1.0, 1.0 };
+GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+
+GLfloat light_position[] = { 1.0, 0.0, 0.0, 0.0 };
+GLfloat light_position2[] = { 0.0, 1.0, 0.0, 0.0 };
+
+
+void init2(void)
+
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_SMOOTH);
+
+    static float lmodel_twoside[] = { GL_TRUE };
+    static float lmodel_oneside[] = { GL_FALSE };
+    static float ambient[] = { 0.2, 0.2, 0.2, 1.0 };
+    static float diffuse[] = { 1.0f, 0.0, 0.0, 1.0 };
+    static float position[] = { -2.0, 1.0, 1.0f, 0.0 };
+
+    static float ambient1[] = { 0.3, 0.3, 0.3, 1.0 };
+    static float diffuse1[] = { 0.0, 1.0, 0.0, 1.0 };
+    static float position1[] = { 1.0,1.0, 1.0f, 0.0 };
+
+    static float ambient2[] = { 0.4, 0.4, 0.4, 1.0 };
+    static float diffuse2[] = { 0.0, 0.0, 1.0, 1.0 };
+    static float position2[] = { 2.0,1.0, 1.0f, 0.0 };
+
+
+    static float front_mat_shininess[] =
+    { 60.0 };
+    static float front_mat_specular[] =
+    { 0.2, 0.2, 0.2, 1.0 };
+    static float front_mat_diffuse[] =
+    { 0.5, 0.5, 0.28, 1.0 };
+    static float back_mat_shininess[] =
+    { 60.0 };
+    static float back_mat_specular[] =
+    { 0.5, 0.5, 0.2, 1.0 };
+    static float back_mat_diffuse[] =
+    { 1.0, 0.9, 0.2, 1.0 };
+
+    static float lmodel_ambient[] =
+    { 1.0, 1.0, 1.0, 1.0 };
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
+
+    glLightfv(GL_LIGHT1, GL_AMBIENT, ambient1);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse1);
+    glLightfv(GL_LIGHT1, GL_POSITION, position1);
+
+    glLightfv(GL_LIGHT2, GL_AMBIENT, ambient2);
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, diffuse2);
+    glLightfv(GL_LIGHT2, GL_POSITION, position2);
+
+    glMaterialfv(GL_FRONT, GL_SHININESS, front_mat_shininess);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, front_mat_specular);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, front_mat_diffuse);
+    glMaterialfv(GL_BACK, GL_SHININESS, back_mat_shininess);
+    glMaterialfv(GL_BACK, GL_SPECULAR, back_mat_specular);
+    glMaterialfv(GL_BACK, GL_DIFFUSE, back_mat_diffuse);
+
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+    glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, lmodel_twoside);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHT2);
+    glShadeModel(GL_SMOOTH);
+}
 GLvoid LoadGLTextures()
 {
     // Загрузка картинки
@@ -57,50 +136,38 @@ void Sun() {
     glEndList();
 }
 
-void Keyboard(unsigned char key, int x, int y)
+void specialKeys(int key, int x, int y)
 {
-    switch (key)
+    if (key == GLUT_KEY_RIGHT)
     {
-    case 'g':
-        gp = TRUE;                    
-        fogfilter += 1;                   
-        if (fogfilter > 2)                 
-
-        {
-            fogfilter = 0;           
-        }
-        glFogi(GL_FOG_MODE, fogMode[fogfilter]); // Режим тумана
-    case 'c':                 
-        gp = FALSE;         
-        break;
+        glDisable(GL_FOG);
+        glClearColor(0.0, 0.0, 0.0, 0.0);
     }
+    else if (key == GLUT_KEY_LEFT)
+    {
+        glEnable(GL_FOG);
+        glFogi(GL_FOG_MODE, GL_LINEAR);
+        glFogfv(GL_FOG_COLOR, fogColor);
+        glFogf(GL_FOG_DENSITY, 2.0f);
+        glFogf(GL_FOG_START, 0.0f);
+        glFogf(GL_FOG_END, 3.0f);
+    }
+
+    glutPostRedisplay();
 }
 void display(void)
 {
 
     int i, j;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3f(0.0, 1.0, 1.0);
-    glEnable(GL_FOG);                       // Включает туман (GL_FOG)
-
-    glFogi(GL_FOG_MODE, fogMode[fogfilter]);// Выбираем тип тумана
-
-    glFogfv(GL_FOG_COLOR, fogColor);        // Устанавливаем цвет тумана
-
-    glFogf(GL_FOG_DENSITY, 0.35f);          // Насколько густым будет туман
-
-    glHint(GL_FOG_HINT, GL_DONT_CARE);      // Вспомогательная установка тумана
-
-    glFogf(GL_FOG_START, 1.0f);             // Глубина, с которой начинается туман
-
-    glFogf(GL_FOG_END, 5.0f);               // Глубина, где туман заканчивается.
+    glColor3f(0.0, 1.0, 1.0);          
     glPushMatrix();
     glRotatef(45.0, 1.0, 1.0, 1.0);
     for (j = 0; j <= 30; j++) {
         glBegin(GL_LINE_STRIP);
         for (i = 0; i <= 100; i++)
             glEvalCoord2f((GLfloat)i / 100.0, (GLfloat)j / 30.0);
-        glEnd();
+        glEnd(); 
         glBegin(GL_LINE_STRIP);
         for (i = 0; i <= 100; i++)
             glEvalCoord2f((GLfloat)j / 30.0, (GLfloat)i / 100.0);
@@ -159,9 +226,10 @@ int main(int argc, char** argv)
     gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 25.0);
     gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
     init();
+    init2();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
-    glutKeyboardFunc(Keyboard);
+    glutSpecialFunc(specialKeys);
     Sun();
     glutMainLoop();
     return 0;
