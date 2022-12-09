c6163a246546b2db53bb802284c2017e34710f3f
shmkvdmd <demidka100@mail.ru>
2022-12-10 00:34:45 +0300

    +

--- 2/SAOD/6LAB/6lab.cpp
+++ 2/SAOD/6LAB/6lab.cpp
@@ -1,303 +1,233 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
-//21 вар
-
-struct node // структура для представления узлов дерева
-{
-	int key;
-	unsigned int height;
-	node* left;
-	node* right;
-	int heightree;
-	node() { height = 1; }
-	node(int k) { key = k; left = right = 0; height = 1; }
+int n; // общее кол-во элементов
+int val; // сам элемент - число
+int choice; // пункт меню, выбранный пользователем
+//структура дерева
+struct tree {
+	int key; // содержание узла
+	struct tree* left; // указатель на младшего потомка
+	struct tree* right; // указатель на старшего потомка
+	int h = 1; //высота
 };
-
-void del(node*& p) //удаление дерева
-{
-	if (p != NULL) {
-		del(p->left);
-		del(p->right);
-		delete p;
-		p = NULL;
-	}
+//проверка ввода
+void is_digit(int& i) {
+	do {
+		cin.ignore(cin.rdbuf()->in_avail());
+		cin >> i;
+	} while (cin.fail());
 }
-
-void counthtree(node* p)//высота дерева
-{
-	if (p) {
-		if (p->right) {
-			p->right->heightree = p->heightree + 1;
-			counthtree(p->right);
-		}
-		if (p->left) {
-			p->left->heightree = p->heightree + 1;
-			counthtree(p->left);
-		}
-	}
+//меню
+void menu(int& choice) {
+	cout << "МЕНЮ:\n";
+	cout << "1) Добавить элемент\n";
+	cout << "2) Удалить элемент\n";
+	cout << "3) Поиск элемента\n";
+	cout << "4) Вывод дерева\n";
+	cout << "5) Вывести список элементов\n";
+	cout << "6)* Вывести список потомков для каждого узла дерева\n";
+	cout << "7) Выход\n";
+	do {
+		is_digit(choice);
+	} while (choice < 1 || choice>7);
 }
-unsigned  int height(node* p)//высота узла
-{
-	return p ? p->height : 0;
+//высота
+int height(tree* R) {
+	if (R != NULL) return R->h;
+	else return 0;
 }
-
-int bfactor(node* p)//разница высот между узлами
-{
-	return height(p->right) - height(p->left);
+//разность высот левого и правого поддеревьев
+int bfactor(tree* R) {
+	return height(R->right) - height(R->left);
 }
-
-void fixheight(node* p)
-{
-	unsigned char hl = height(p->left);
-	unsigned char hr = height(p->right);
-	p->height = (hl > hr ? hl : hr) + 1;
+//восстанавливает корректное значение поля height заданного узла 
+void fixheight(tree* R) {
+	int hl = height(R->left);
+	int hr = height(R->right);
+	if (hl > hr) R->h = hl + 1;
+	else R->h = hr + 1;
 }
-
-node* rotateright(node* p) // правый поворот вокруг p
-{
-	node* q = p->left;
+// правый поворот вокруг q
+tree* right_rotate(tree* p) {
+	tree* q = p->left;
 	p->left = q->right;
 	q->right = p;
 	fixheight(p);
 	fixheight(q);
 	return q;
 }
-
-node* rotateleft(node* q) // левый поворот вокруг q
-{
-	node* p = q->right;
+// левый поворот вокруг p
+tree* left_rotate(tree* q) {
+	tree* p = q->right;
 	q->right = p->left;
 	p->left = q;
 	fixheight(q);
 	fixheight(p);
 	return p;
 }
-
-node* balance(node* p) // балансировка узла p
-{
-	fixheight(p);
-	if (bfactor(p) == 2)
-	{
-		if (bfactor(p->right) < 0)
-			p->right = rotateright(p->right);
-		return rotateleft(p);
+// балансировка узла
+tree* balance(tree* R) {
+	fixheight(R);
+	if (bfactor(R) == 2) {
+		if (bfactor(R->right) < 0)
+			R->right = right_rotate(R->right);
+		return left_rotate(R);
 	}
-	if (bfactor(p) == -2)
-	{
-		if (bfactor(p->left) > 0)
-			p->left = rotateleft(p->left);
-		return rotateright(p);
+	if (bfactor(R) == -2) {
+		if (bfactor(R->left) > 0)
+			R->left = left_rotate(R->left);
+		return right_rotate(R);
 	}
-	return p; // балансировка не нужна
+	return R; // балансировка не нужна
 }
-
-void insert(node*& p, int k) // вставка ключа k в дерево с корнем p
-{
-	if (p == NULL)
-	{
-		node* head = new node;
-		head->key = k;
-		head->right = NULL;
-		head->left = NULL;
-		p = head;
-		return;
+//ввод
+tree* push(tree*& R, int val) {
+	if (R == NULL) {
+		tree* root = new tree;
+		root->key = val;
+		root->right = NULL;
+		root->left = NULL;
+		R = root;
+		return R;
 	}
-	if (k == p->key) {
-		cout << "Такой элемент уже есть" << endl;
-		return;
+	if (val < R->key) push(R->left, val);
+	else push(R->right, val);
+	R = balance(R);
+}
+tree * search(tree * &R, int val, int k) {
+	if (val == R->key) {
+		cout << "Кол-во шагов: " << k++ << endl;
+		return R;
 	}
-	if (k < p->key)
-		insert(p->left, k);
-	else
-		insert(p->right, k);
-
-	p = balance(p);
-}
-
-node* findmin(node* p) // поиск узла с минимальным ключом в дереве p  (самый малый элемент это левая ветка левого поддерева)
-{
-	return p->left ? findmin(p->left) : p;
-}
-
-node* removemin(node* p) // удаление узла с минимальным ключом из дерева p
-{
-	if (p->left == 0)
-		return p->right;
-	p->left = removemin(p->left);
-	return balance(p);
-}
-
-node* remove(node* p, int k) // удаление ключа k из дерева p
-{
-	if (!p) {
-		return 0;
+	k++;
+	if (val < R->key) {
+		if (R->left == NULL) {
+			tree* R = new tree();
+			return NULL;
+		}
+		search(R->left, val, k);
 	}
-	p->left = remove(p->left, k);
-	if (k == p->key)
-	{
-		node* q = p->left;
-		node* r = p->right;
-		delete p;
-		if (!r) return q;
-		node* min = findmin(r);
-		min->right = removemin(r);
-		min->left = q;
-		cout << "Элемент удален\n";
+	else {
+		if (R->right == NULL) {
+			tree* R = new tree();
+			return NULL;
+		}
+		search(R->right, val, k);
+	}
+}
+tree* find_min(tree* R) {
+	if (R->left == NULL) return R;
+	else find_min(R->left);
+}
+tree* remmin(tree* R) {
+	if (R->left == NULL) return R->right;
+	R->left = remmin(R->left);
+	return balance(R);
+}
+tree* remove(tree* R, int val) {
+	if (R == NULL) return 0;
+	if (val < R->key)
+		R->left = remove(R->left, val);
+	else if (val > R->key)
+		R->right = remove(R->right, val);
+	else {
+		tree* l = R->left;
+		tree* r = R->right;
+		delete R;
+		if (r == NULL) return l;
+		tree* min = find_min(r);
+		min->right = remmin(r);
+		min->left = l;
 		return balance(min);
 	}
-	p->right = remove(p->right, k);
-	return balance(p);
+	return balance(R);
 }
-
-void find(node* p, int k, int& counter, bool& d) //поиск элементов
-{
-	counter++;
-	if (p == NULL)
-		return;
-	if (k == p->key)
-	{
-		d = true;
-		return;
-	}
-	if (k < p->key)
-		find(p->left, k, counter, d);
-	else
-	{
-		find(p->right, k, counter, d);
+//вывод дерева
+void print_tree(tree* R, int l) {
+	if (R != NULL) {
+		print_tree(R->right, l + 1);
+		for (int i = 1; i <= l; i++) cout << " ";
+		cout << R->key << endl;
+		print_tree(R->left, l + 1);
 	}
 }
-
-void output(node* h, int i, bool flag) //вывод элементоа
-{
-	if (h == NULL)
-	{
-		return;
-	}
-	if (flag)
-		i += 5;
-	output(h->left, i, flag);
-	cout << setw(65 - i) << h->key << endl;
-	output(h->right, i, flag);
+void print_child(tree* R, tree* parent) {
+	if (R == NULL || R == parent) return;
+	print_child(R->left, parent);
+	cout << R->key << " ";
+	print_child(R->right, parent);
 }
-
-void task(node* p, node*& hmax, node*& hmin) //поиск элементов с минимальной и максимальной глубиной
-{
-	if (p != NULL)
-	{
-		if (p->left == NULL && p->right == NULL)
-		{
-			if (hmax) {
-				if (p->heightree > hmax->heightree)
-					hmax = p;
-			}
-			else {
-				hmax = p;
-			}
-			if (hmin) {
-				if (p->heightree < hmin->heightree)
-					hmin = p;
-			}
-			else {
-				hmin = p;
-			}
-		}
-		task(p->left, hmax, hmin);
-		task(p->right, hmax, hmin);
+void print_parent(tree* parent) {
+	cout << "Родитель: " << parent->key << ". Потомки: ";
+	if (parent->left != NULL) {
+		tree* child = parent->left;
+		print_child(child, parent);
 	}
-
-}
-void inorder(node* p) //симметричный обход
-{
-	if (p != NULL)
-	{
-		cout << p->key << "\t";
-		inorder(p->left);
-		inorder(p->right);
+	if (parent->right != NULL) {
+		tree* child = parent->right;
+		print_child(child, parent);
 	}
+	cout << endl;
 }
-int main()
-{
+void prev(tree* R) {
+	if (R == NULL || (R->left == NULL && R->right == NULL)) return;
+	print_parent(R);
+	prev(R->left);
+	prev(R->right);
+}
+void print_arr(tree* R) {
+	if (R == NULL) return;
+	cout << R->key << " ";
+	print_arr(R->left);
+	print_arr(R->right);
+}
+int main() {
 	setlocale(LC_ALL, "rus");
-	node* head = new node(0);
-	head = NULL;
-	bool flag = true;
-	node* hmax = NULL;
-	node* hmin = NULL;
-
-	while (flag) {
-		bool findflag = false;
-		int counter = 0;
-		int v;
-		cout << endl;
-		cout << "1.Добавление элемента\n"
-			<< "2.Удаление элемента\n"
-			<< "3.Показать дерево\n"
-			<< "4.Поиск элемента\n"
-			<< "5.Задание\n"
-			<< "6.Выход\n";
-		cin >> v;
-		cin.clear();
-		cin.ignore(cin.rdbuf()->in_avail());
-		switch (v)
-		{
-		case 1:
-
-			cout << "Введите добавляемое число \n";
-			int numadd;
-			cin >> numadd;
-			insert(head, numadd);
-			break;
-
-		case 2:
-			cout << "Введите удаляемое число\n";
-			int numdel;
-			cin >> numdel;
-			head = remove(head, numdel);
-			break;
-
-		case 3:
-
-			if (head)
-				output(head, 0, true);
-			else
-				cout << "Дерево пустое " << endl;
-			break;
-
-		case 4:
-			cout << "Введите число для поиска:\n";
-			int numfind;
-			cin >> numfind;
-			findflag = false;
-			find(head, numfind, counter, findflag);
-			if (findflag) {
-				cout << "Найдено\n";
-				cout << "Количество шагов: " << counter << endl;
+	tree* root = NULL; // ячейка для корня
+	while (1) {
+		menu(choice);
+		// добавить
+		if (choice == 1) {
+			cout << "Введите новый элемент:" << endl;
+			is_digit(val);
+			if (root != NULL) {
+				if (search(root, val, 0) != NULL) cout << "Одинаковых элементов не должно быть в нашем дереве!!!\n";
+				else root = push(root, val);
 			}
-			else
-				cout << "Этого элемента нет в дереве\n";
-			break;
-
-		case 5:
-		{
-			if (head)
-			{
-				cout << "\n";
-				cout << "Прямой обход дерева:\n" << endl;
-				inorder(head);
-				cout << "\n";
-				cout << "\n";
+			else root = push(root, val);
+		}
+		// удалить
+		else if (choice == 2) {
+			cout << "Введите элемент, который хотите удалить:" << endl;
+			is_digit(val);
+			root = remove(root, val);
+			if (root == NULL) {
+				cout << "Удалено всё дерево\n";
+				break;
 			}
 		}
-			break;
-		case 6:
-			del(head);
-			flag = false;
-			break;
-		default:
-			cout << "Введите заново!\n";
-			break;
+		// поиск
+		else if (choice == 3) {
+			cout << "Введите элемент, который надо найти:" << endl;
+			is_digit(val);
+			if (search(root, val, 0) != NULL) cout << "Элемент найден\n";
+			else cout << "Элемент не найден\n";
 		}
+			// вывод дерева
+		else if (choice == 4) {
+			print_tree(root, 0);
+		}
+		// вывод списка
+		else if (choice == 5) {
+			cout << "Список, соответствующий прямому обходу:\n";
+			print_arr(root);
+			cout << endl;
+		}
+		// вывод потомков
+		else if (choice == 6) {
+			prev(root);
+		}
+		else if (choice == 7) break;
 	}
 	return 0;
 }
