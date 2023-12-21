9d45cb06301f8600fec6727c0e7e6405a0512c1c
shmkvdmd <demidka100@mail.ru>
2023-12-21 22:39:42 +0300

    !

--- 3 year/DataBase/6lab/Subqueries.sql
+++ 3 year/DataBase/6lab/Subqueries.sql
@@ -20,31 +20,31 @@ AND started <= ALL (
 	WHERE crit_name = 'Критический'
 );
 -- д. Тестировщик, с самым большим количеством добавленных багов
+SELECT id_tester, t_first_name, t_surname, t_lastname
+FROM tester
+WHERE id_tester = (
+    SELECT id_tester
+    FROM bug
+    GROUP BY id_tester
+    ORDER BY COUNT(id_bug) DESC
+    LIMIT 1
+);
+
 SELECT t.id_tester, t.t_first_name, t.t_surname, t.t_lastname FROM tester t
-WHERE t.id_tester NOT IN (
-    SELECT b.id_tester FROM bug b
-    GROUP BY b.id_tester
-    HAVING COUNT(b.id_bug) < (
-        SELECT COUNT(b2.id_bug) FROM bug b2
-        WHERE b2.id_tester = t.id_tester
+WHERE NOT EXISTS (
+    SELECT 1 FROM bug b1
+    WHERE b1.id_tester = t.id_tester
+) OR NOT EXISTS (
+    SELECT 1 FROM bug b2
+    WHERE b2.id_tester = t.id_tester
+    GROUP BY b2.id_tester
+    HAVING COUNT(b2.id_bug) < (
+        SELECT COUNT(b3.id_bug) FROM bug b3
+        WHERE b3.id_tester = t.id_tester
     )
 )
 LIMIT 1;
 
-SELECT t.id_tester, t.t_first_name, t.t_surname, t.t_lastname FROM tester t
-EXCEPT
-SELECT t.id_tester, t.t_first_name, t.t_surname, t.t_lastname FROM tester t
-JOIN bug b ON t.id_tester = b.id_tester
-GROUP BY t.id_tester
-HAVING COUNT(b.id_bug) > 0
-ORDER BY COUNT(b.id_bug) DESC
-LIMIT 1;
-
-SELECT t.id_tester, t.t_first_name, t.t_surname, t.t_lastname FROM tester t
-LEFT JOIN bug b ON t.id_tester = b.id_tester
-GROUP BY t.id_tester
-ORDER BY COUNT(b.id_bug) DESC
-LIMIT 1;
 
 -- е. Разработчик, у которого нет неисправленных багов
 
